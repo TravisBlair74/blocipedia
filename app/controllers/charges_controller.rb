faa3d089ca0ff8fe80fde68b45ce23892d1ecf0f
Travis-Blair <travisblair74@gmail.com>
2018-02-05 14:41:57 -0500

    Config update

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,31 @@
+class ChargesController < ApplicationController
+  def create
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: Amount.default,
+      description: "BigMoney Membership - #{current_user.email}",
+      currency: 'usd'
+    )
+
+    flash[:notice] = "Thanks for the payment, #{current_user.email}!"
+    current_user.role = 'premium'
+    redirect_to user_path(current_user)
+
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_charge_path
+  end
+
+  def new
+  @stripe_btn_data = {
+    key: "#{ Rails.configuration.stripe[:publishable_key] }",
+    description: "BigMoney Membership - #{current_user.email}",
+    amount: 15_00
+  }
+  end
+end
